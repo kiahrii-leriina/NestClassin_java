d044443f3f6eeab0d9450333097a9a05319a5397
kaihrii leriina <litetencent5@gmail.com>
2025-07-06 16:18:42 +0530

    second commit

--- src/MyPackage/Main.java
+++ src/MyPackage/Main.java
@@ -14,9 +14,24 @@ class A {
 		public void show() {
 			System.out.println("value of b: "+b);
 		}
+		
+		public static class C{
+			int c = 15;
+			public void show() {
+				System.out.println("value of c: "+c);
+			}
+		}
 	
 	}
+	
+	static class D{
+		int d=20;
+		public void show() {
+			System.out.println("value of d: "+d);
+		}
+	}
 }
+
 
 public class Main {
 	
@@ -26,8 +41,16 @@ public class Main {
 		obj.show();
 		
 		A.B obj1 = obj.new B();
-		
 		obj1.show();
+		
+		A.D obj2 = new A.D();
+		obj2.show();
+		
+		
+		A.B.C obj3 = new A.B.C();
+		obj3.show();
+		
+		
 		
 	}
 	
